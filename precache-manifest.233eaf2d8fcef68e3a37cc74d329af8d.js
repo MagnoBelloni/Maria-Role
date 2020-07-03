ec133a540419aae2ad623b0294c1b30065a8f366
Magno Belloni <bellonidesouza6@gmail.com>
2020-07-03 18:00:50 -0300

    Updates

---
+++ precache-manifest.233eaf2d8fcef68e3a37cc74d329af8d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5cafef05f7240ee65974c873e3db93",
+    "url": "/maria-role/index.html"
+  },
+  {
+    "revision": "5d975d799e1e4a899dbc",
+    "url": "/maria-role/static/js/2.469effcb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/maria-role/static/js/2.469effcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00525c81f7f1971dd109",
+    "url": "/maria-role/static/js/main.0c9f8073.chunk.js"
+  },
+  {
+    "revision": "40dce49de5ea9071d9af",
+    "url": "/maria-role/static/js/runtime-main.fcd8998e.js"
+  },
+  {
+    "revision": "60abe7919432b3d3bf87fe2cb38da529",
+    "url": "/maria-role/static/media/background3.60abe791.jpeg"
+  },
+  {
+    "revision": "9308cc54b274747e41f6df9d7ebbb889",
+    "url": "/maria-role/static/media/delivery-man.9308cc54.png"
+  },
+  {
+    "revision": "13183a3a3b00fe40c0f4e7861e8c6b62",
+    "url": "/maria-role/static/media/double_cellphone_greeting.13183a3a.png"
+  },
+  {
+    "revision": "592df80296000b7f1cd23eaaef549b0d",
+    "url": "/maria-role/static/media/logo.592df802.png"
+  },
+  {
+    "revision": "65c0724fc79a5a0a92526cf65cfea0c5",
+    "url": "/maria-role/static/media/welcome_how_it_works_02.65c0724f.png"
+  }
+]);
